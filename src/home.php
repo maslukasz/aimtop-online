f2e0c0173eda1c0e18632876754f6d8040be03e4
maslukasz <lukesekmc@gmail.com>
2024-07-15 09:27:42 +0200

    color change

--- src/home.php
+++ src/home.php
@@ -38,7 +38,7 @@
                 class="absolute inset-x-0 top-1/2 h-px -translate-y-1/2 bg-transparent bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75">
             </div>
 
-            <span class="relative z-10 bg-gray-800 px-6 text-yellow-300/50">Our content</span>
+            <span class="relative z-10 bg-gray-800 px-6 text-yellow-300">Our content</span>
         </span>
         <div class="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-8 p-6">
             <div
