6d1cbb2a5bf5fa89b06e2c85abf6c194f9673558
maslukasz <lukesekmc@gmail.com>
2024-08-10 22:50:44 +0200

    profile fix

--- src/profile.php
+++ src/profile.php
@@ -15,6 +15,7 @@ $ab2_rank = $u->getAimbeastRank2($u->name);
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../public/css/output.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
 </head>
